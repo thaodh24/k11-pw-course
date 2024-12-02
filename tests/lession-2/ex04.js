dc9aa4016f8750bcb73829ee0fb3c6ff80e5c98d
Thao <huongthao241297@gmail.com>
2024-12-02 15:36:46 +0700

    feat: add ex04.js

---
+++ tests/lession-2/ex04.js
@@ -0,0 +1,7 @@
+let yourHeight = 164;
+let dreamWeight = (yourHeight * 9) / 10;
+let maxWeight = yourHeight - 100;
+let minWeight = (yourHeight * 8) / 10;
+
+console.log("Chiều cao của bạn là " + yourHeight + "cm");
+console.log(dreamWeight + "kg " + maxWeight + "kg " + minWeight + "kg ");
