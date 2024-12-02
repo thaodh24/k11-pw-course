754e0510430dba9f6ffda1c993bb49ac0c2c26c5
Thao <huongthao241297@gmail.com>
2024-12-02 15:48:04 +0700

    fix: ex05.js fix false logic code

--- tests/lession-2/ex05.js
+++ tests/lession-2/ex05.js
@@ -1,4 +1,4 @@
-for (let i = 1; i > 100; i++) {
+for (let i = 1; i <= 100; i++) {
     if (i % 2 === 0) { console.log("Số " + i + " là số chẵn") };
     if (i % 2 === 1) { console.log("Số" + i + " là số lẻ") };
 };
